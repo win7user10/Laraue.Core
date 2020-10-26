7714986c717aca4d232a9fa5e6067a015128db47
Ilya Belyanskiy <win7user10@yandex.ru>
2020-10-26 16:51:53 +0400

    Stored procedures concept, part 15.

--- tests/Laraue.Core.Tests/StoredProcedures/TestDbContext.cs
+++ tests/Laraue.Core.Tests/StoredProcedures/TestDbContext.cs
@@ -1,9 +1,12 @@
 ﻿using EFSqlTranslator.EFModels;
 using EFSqlTranslator.Translation;
 using EFSqlTranslator.Translation.DbObjects.PostgresQlObjects;
+using EFSqlTranslator.Translation.DbObjects.SqlObjects;
+using EFSqlTranslator.Translation.Extensions;
 using Laraue.Core.DataAccess.StoredProcedures;
 using Laraue.Core.DataAccess.StoredProcedures.Common;
 using Laraue.Core.DataAccess.StoredProcedures.CSharpBuilder;
+using Laraue.Core.Extensions.Linq;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.EntityFrameworkCore.Design.Internal;
@@ -12,12 +15,14 @@ using Microsoft.EntityFrameworkCore.Internal;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Migrations.Design;
 using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using Microsoft.EntityFrameworkCore.Query.Internal;
 using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Text.RegularExpressions;
 using Xunit;
 
 namespace Laraue.Core.Tests.StoredProcedures
@@ -54,12 +59,39 @@ namespace Laraue.Core.Tests.StoredProcedures
         [Fact]
         public void DoSmth()
         {
-
-            Expression<Func<User, bool>> exp1 = (User x) => x.Name.Contains("a");
-            var query = _dbContext.Users.Where(exp1);
-
             var modelInfoProvider = new EFModelInfoProvider(_dbContext);
-            var translatedExpression = QueryTranslator.Translate(query.Expression, modelInfoProvider, new PostgresQlObjectFactory());
+
+
+
+            Expression<Func<User, Transaction, bool>> exp2 = (User x, Transaction y) => x.Id == y.UserId;
+
+            // Need to create two expressions
+            // 1 - condition expression
+            // 2 - action expression
+
+            // Parse each lambda, if has parameter of type (T2), replace it to string @NEW.MemberName@
+
+            // Part 1
+            var conditionRightParameter = exp2.Parameters[1]; // int, UserId
+            var conditionRightExpression = (MemberExpression)((BinaryExpression)exp2.Body).Right;
+            var conditionRightMemberName = conditionRightExpression.Member.Name;
+            var conditionRightEntityProperty = _dbContext.Model.FindEntityType(typeof(Transaction)).FindProperty(conditionRightMemberName);
+            var conditionRightEntityPropertyColumnName = conditionRightEntityProperty.GetColumnName();
+            var rightPartQuery = $"NEW.{conditionRightEntityPropertyColumnName}";
+
+            // Part 2
+            var conditionLeftParameter = exp2.Parameters[0]; // User
+            var conditionLeftExpression = (MemberExpression)((BinaryExpression)exp2.Body).Left;
+            var defaultConditionParameterValue = GetDefault(conditionLeftExpression.Type);
+            var conditionLeftBinaryExpression = Expression.MakeBinary(ExpressionType.Equal, conditionLeftExpression, Expression.Constant(defaultConditionParameterValue));
+            var conditionLeftComiledExpression = Expression.Lambda<Func<User, bool>>(conditionLeftBinaryExpression, conditionLeftParameter);
+
+            // Part 3 
+            var query = _dbContext.Users.Where(conditionLeftComiledExpression);
+            var translatedExpression = QueryTranslator.Translate(query.Expression, modelInfoProvider, new PostgresQlObjectFactory()).ToString();
+
+            // Part 4
+            var replacedExpression = Regex.Replace(translatedExpression, $"= {defaultConditionParameterValue}", $"= {rightPartQuery}");
 
             var generator = new DataAccess.StoredProcedures.CSharpBuilder.CSharpMigrationOperationGenerator(
                 new CSharpMigrationOperationGeneratorDependencies(
@@ -76,6 +108,16 @@ namespace Laraue.Core.Tests.StoredProcedures
                 new object[] { "user.balance + NEW.balance" }
             ) }, builder);
         }
+
+        public object GetDefault(Type t)
+        {
+            return GetType().GetMethod("GetDefaultGeneric").MakeGenericMethod(t).Invoke(this, null);
+        }
+
+        public T GetDefaultGeneric<T>()
+        {
+            return default;
+        }
     }
 
     public class TestDbContext : DbContext
@@ -85,6 +127,7 @@ namespace Laraue.Core.Tests.StoredProcedures
         public DbSet<Transaction> Transactions { get; set; }
 
         public TestDbContext(DbContextOptions<TestDbContext> options) : base(options)
+
         { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
