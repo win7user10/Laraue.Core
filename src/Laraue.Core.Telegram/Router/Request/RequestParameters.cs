43440dca7b65898d02c5e4b7afd5924a781e1e69
Ilya Belyanskiy <win7user20@gmail.com>
2022-08-06 23:26:57 +0300

    added GetValueOrDefault method to the request parameters

--- src/Laraue.Core.Telegram/Router/Request/RequestParameters.cs
+++ src/Laraue.Core.Telegram/Router/Request/RequestParameters.cs
@@ -1,27 +1,52 @@
-﻿namespace Laraue.Core.Telegram.Router.Request;
+﻿using System.Text.Json;
+
+namespace Laraue.Core.Telegram.Router.Request;
 
 public class RequestParameters : Dictionary<string, string?>
 {
     public RequestParameters(IDictionary<string, string?> source)
-        : base(source)
+        : base(source, StringComparer.OrdinalIgnoreCase)
     { 
     }
     
-    public RequestParameters()
+    public RequestParameters() : base(StringComparer.OrdinalIgnoreCase)
     { 
     }
     
+    /// <summary>
+    /// Get "page" parameter value, the min value is 1.
+    /// </summary>
     public int Page
     {
         get
         {
-            var intPage = 0;
-            if (TryGetValue("page", out var page))
-            {
-                _ = int.TryParse(page, out intPage);
-            }
+            var page = GetValueOrDefault<int>("page");
 
-            return intPage > 0 ? intPage : 1;
+            return page > 0 ? page : 1;
         }
+    }
+    
+    /// <summary>
+    /// Return parameter value or default value.
+    /// </summary>
+    /// <param name="parameterName"></param>
+    /// <param name="defaultValue"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public T? GetValueOrDefault<T>(string parameterName, T? defaultValue = default)
+    {
+        if (!TryGetValue(parameterName, out var value))
+        {
+            return defaultValue;
+        }
+        
+        if (typeof(T) == typeof(string))
+        {
+            return (dynamic?) value;
+        }
+
+        return value is null 
+            ? defaultValue
+            : JsonSerializer.Deserialize<T>(value);
     }
 }
