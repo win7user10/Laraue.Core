76868d3c93e37a05cc3de3e858441ea39372cae5
Mark Cilia Vincenti <markciliavincenti@gmail.com>
2023-01-15 11:35:05 +0100

    Switched to AsyncKeyedLock for better performance and less allocations.

--- src/Laraue.Core.Threading/KeyedSemaphoreSlim.cs
+++ src/Laraue.Core.Threading/KeyedSemaphoreSlim.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
+﻿using AsyncKeyedLock;
+using System;
+using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -12,7 +12,7 @@ namespace Laraue.Core.Threading
     /// <typeparam name="TKey"></typeparam>
     public class KeyedSemaphoreSlim<TKey>
     {
-        private readonly SemaphoreWrappersDictionary _semaphores;
+        private readonly AsyncKeyedLocker<TKey> _asyncKeyedLocker;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="KeyedSemaphoreSlim{TKey}"/> class, specifying
@@ -20,9 +20,15 @@ namespace Laraue.Core.Threading
         /// </summary>
         /// <param name="initCount"></param>
         /// <param name="maxCount"></param>
+        [Obsolete("Passing on an initCount different to maxCount could have undesired results. Only pass on initCount instead.")]
         public KeyedSemaphoreSlim(int initCount, int maxCount)
         {
-            _semaphores = new SemaphoreWrappersDictionary(initCount, maxCount);
+            _asyncKeyedLocker = new AsyncKeyedLocker<TKey>(o =>
+            {
+                o.MaxCount = initCount;
+                o.PoolSize = 20;
+                o.PoolInitialFill = 1;
+            });
         }
         
         /// <summary>
@@ -30,98 +36,37 @@ namespace Laraue.Core.Threading
         /// the initial number of requests that can be granted concurrently.
         /// </summary>
         /// <param name="initCount"></param>
-        public KeyedSemaphoreSlim(int initCount) : this(initCount, initCount)
+        public KeyedSemaphoreSlim(int initCount)
         {
+            _asyncKeyedLocker = new AsyncKeyedLocker<TKey>(o =>
+            {
+                o.MaxCount = initCount;
+                o.PoolSize = 20;
+                o.PoolInitialFill = 1;
+            });
         }
-        
+
         /// <summary>
         /// Asynchronously waits to enter the <see cref="KeyedSemaphoreSlim{TKey}"/>
         /// </summary>
         /// <param name="key"></param>
         /// <param name="ct"></param>
         /// <returns>Object to release <see cref="KeyedSemaphoreSlim{TKey}"/></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public async Task<IDisposable> WaitAsync(TKey key, CancellationToken ct = default)
         {
-            var semaphoreSlimWrapper = _semaphores.GetAndIncrementRefCount(key);
-            await semaphoreSlimWrapper.SemaphoreSlim.WaitAsync(ct).ConfigureAwait(false);
-            return new SemaphoreReleaser(key, _semaphores);
+            return await _asyncKeyedLocker.LockAsync(key, ct).ConfigureAwait(false);
         }
-        
+
         /// <summary>
         /// Blocks the current thread until it can enter the <see cref="KeyedSemaphoreSlim{TKey}"/>.
         /// </summary>
         /// <param name="key"></param>
         /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public IDisposable Wait(TKey key)
         {
-            var semaphoreSlimWrapper = _semaphores.GetAndIncrementRefCount(key);
-            semaphoreSlimWrapper.SemaphoreSlim.Wait();
-            return new SemaphoreReleaser(key, _semaphores);
-        }
-
-        private sealed class SemaphoreWrappersDictionary : ConcurrentDictionary<TKey, SemaphoreWrapper>
-        {
-            private readonly int _initCount;
-            private readonly int _maxCount;
-
-            public SemaphoreWrappersDictionary(int initCount, int maxCount)
-            {
-                _initCount = initCount;
-                _maxCount = maxCount;
-            }
-            
-            public SemaphoreWrapper GetAndIncrementRefCount(TKey key)
-            {
-                lock (this)
-                {
-                    var semaphore = GetOrAdd(key, new SemaphoreWrapper(_initCount, _maxCount));
-                    semaphore.RefsCount++;
-                    return semaphore;
-                }
-            }
-        }
-        
-        private sealed class SemaphoreReleaser : IDisposable
-        {
-            private readonly TKey _key;
-            private readonly SemaphoreWrappersDictionary _semaphoreWrappersDictionary;
-
-            public SemaphoreReleaser(TKey key, SemaphoreWrappersDictionary semaphoreWrappersDictionary)
-            {
-                _key = key;
-                _semaphoreWrappersDictionary = semaphoreWrappersDictionary;
-            }
-
-            public void Dispose()
-            {
-                lock (_semaphoreWrappersDictionary)
-                {
-                    if (!_semaphoreWrappersDictionary.TryGetValue(_key, out var semaphoreWrapper))
-                    {
-                        return;
-                    }
-                    
-                    semaphoreWrapper.RefsCount--;
-                    if (semaphoreWrapper.RefsCount == 0)
-                    {
-                        _semaphoreWrappersDictionary.Remove(_key, out _);
-                    }
-
-                    semaphoreWrapper.SemaphoreSlim.Release();
-                }
-            }
-        }
-
-        private class SemaphoreWrapper
-        {
-            public int RefsCount { get; set; }
-
-            public SemaphoreSlim SemaphoreSlim { get; }
-
-            public SemaphoreWrapper(int initCount, int maxCount)
-            {
-                SemaphoreSlim = new SemaphoreSlim(initCount, maxCount);
-            }
+            return _asyncKeyedLocker.Lock(key);
         }
     }
 }
