42adc9ba3c08d3740e4d9f9210674ff38a382813
Ilya Belyanskiy <win7user20@gmail.com>
2023-03-14 22:46:30 +0300

    add project for utils to work with datetime

---
+++ src/Laraue.Core.DateTime/Services/Abstractions/IDateTimeProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Laraue.Core.DateTime.Services.Abstractions;
+
+/// <summary>
+/// Mock for the date time provider.
+/// </summary>
+public interface IDateTimeProvider
+{
+    /// <summary>
+    /// Returns current <see cref="DateTime"/> the time. 
+    /// </summary>
+    public System.DateTime UtcNow { get; }
+    
+    /// <summary>
+    /// Returns current <see cref="DateTimeOffset"/>.
+    /// </summary>
+    public DateTimeOffset UtcOffsetNow { get; }
+}
---
+++ src/Laraue.Core.DateTime/Services/Impl/DateTimeProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Laraue.Core.DateTime.Services.Abstractions;
+
+namespace Laraue.Core.DateTime.Services.Impl;
+
+/// <inheritdoc />
+public class DateTimeProvider : IDateTimeProvider
+{
+    /// <inheritdoc />
+    public System.DateTime UtcNow => System.DateTime.UtcNow;
+    
+    /// <inheritdoc />
+    public DateTimeOffset UtcOffsetNow => DateTimeOffset.UtcNow;
+}
