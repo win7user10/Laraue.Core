4245494321fe4bb62124ec09a15c632b912542d7
Ilya Belyanskiy <win7user20@gmail.com>
2023-05-07 20:25:02 +0300

    DB context interface for EF core

---
+++ src/Laraue.Core.DataAccess.EFCore/IDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace Laraue.Core.DataAccess.EFCore;
+
+/// <summary>
+/// EF Core DB context.
+/// </summary>
+public interface IDbContext
+{
+    /// <summary>Save changes method.</summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Provides access to information and operations for entity instances this context is tracking.
+    /// </summary>
+    ChangeTracker ChangeTracker { get; }
+}
