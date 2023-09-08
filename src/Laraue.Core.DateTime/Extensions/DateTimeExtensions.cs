3d38988ef244617bd3f83431a727e5d3a0f8a61e
DESKTOP-KT6GSMQ\Ilya <win7user10@yandex.ru>
2023-09-08 18:02:11 +0300

    add some new DateTime extensions

--- src/Laraue.Core.DateTime/Extensions/DateTimeExtensions.cs
+++ src/Laraue.Core.DateTime/Extensions/DateTimeExtensions.cs
@@ -27,4 +27,64 @@ public static class DateTimeExtensions
     {
         return System.DateTime.SpecifyKind(dateTime, DateTimeKind.Utc);
     }
+    
+    /// <summary>
+    /// Return the same date without seconds.
+    /// </summary>
+    /// <param name="dateTime"></param>
+    /// <returns></returns>
+    public static System.DateTime StartOfMinute(this System.DateTime dateTime)
+    {
+        return new System.DateTime(dateTime.Year, dateTime.Month, dateTime.Day, dateTime.Hour, dateTime.Minute, 0, dateTime.Kind);
+    }
+    
+    /// <summary>
+    /// Return the same date without minutes.
+    /// </summary>
+    /// <param name="dateTime"></param>
+    /// <returns></returns>
+    public static System.DateTime StartOfHour(this System.DateTime dateTime)
+    {
+        return new System.DateTime(dateTime.Year, dateTime.Month, dateTime.Day, dateTime.Hour, 0, 0, dateTime.Kind);
+    }
+    
+    /// <summary>
+    /// Return the same date without hours.
+    /// </summary>
+    /// <param name="dateTime"></param>
+    /// <returns></returns>
+    public static System.DateTime StartOfDay(this System.DateTime dateTime)
+    {
+        return new System.DateTime(dateTime.Year, dateTime.Month, dateTime.Day, 0, 0, 0, dateTime.Kind);
+    }
+    
+    /// <summary>
+    /// Return the date with seconds updated to 59.
+    /// </summary>
+    /// <param name="dateTime"></param>
+    /// <returns></returns>
+    public static System.DateTime EndOfMinute(this System.DateTime dateTime)
+    {
+        return new System.DateTime(dateTime.Year, dateTime.Month, dateTime.Day, dateTime.Hour, dateTime.Minute, 59, dateTime.Kind);
+    }
+    
+    /// <summary>
+    /// Return the date with minutes and seconds updated to 59.
+    /// </summary>
+    /// <param name="dateTime"></param>
+    /// <returns></returns>
+    public static System.DateTime EndOfHour(this System.DateTime dateTime)
+    {
+        return new System.DateTime(dateTime.Year, dateTime.Month, dateTime.Day, dateTime.Hour, 59, 59, dateTime.Kind);
+    }
+    
+    /// <summary>
+    /// Return the date with hours updated to 23, minutes and seconds updated to 59.
+    /// </summary>
+    /// <param name="dateTime"></param>
+    /// <returns></returns>
+    public static System.DateTime EndOfDay(this System.DateTime dateTime)
+    {
+        return new System.DateTime(dateTime.Year, dateTime.Month, dateTime.Day, 23, 59, 59, dateTime.Kind);
+    }
 }
