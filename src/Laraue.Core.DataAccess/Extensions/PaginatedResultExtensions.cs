41e53b398a64205687a79f37b4315ae5707f7c52
DESKTOP-KT6GSMQ\Ilya <win7user10@yandex.ru>
2023-12-12 14:41:42 +0300

    add MapTo overload for IShortPaginatedResult

--- src/Laraue.Core.DataAccess/Extensions/PaginatedResultExtensions.cs
+++ src/Laraue.Core.DataAccess/Extensions/PaginatedResultExtensions.cs
@@ -4,16 +4,14 @@ using System.Linq;
 
 namespace Laraue.Core.DataAccess.Extensions
 {
+    /// <summary>
+    /// Utils to modify paginated requests.
+    /// </summary>
     public static class PaginatedResultExtensions
     {
         /// <summary>
         /// Convert all elements of paginated result, using passed function.
         /// </summary>
-        /// <typeparam name="TTo"></typeparam>
-        /// <typeparam name="TFrom"></typeparam>
-        /// <param name="fullPaginatedResult"></param>
-        /// <param name="convert"></param>
-        /// <returns></returns>
         public static IFullPaginatedResult<TTo> MapTo<TTo, TFrom>(
             this IFullPaginatedResult<TFrom> fullPaginatedResult,
             Func<TFrom, TTo> convert)
@@ -25,6 +23,22 @@ namespace Laraue.Core.DataAccess.Extensions
                 fullPaginatedResult.PerPage,
                 fullPaginatedResult.Total,
                 fullPaginatedResult.Data.Select(convert).ToList());
+        }
+        
+        /// <summary>
+        /// Convert all elements of paginated result, using passed function.
+        /// </summary>
+        public static IShortPaginatedResult<TTo> MapTo<TTo, TFrom>(
+            this IShortPaginatedResult<TFrom> shortPaginatedResult,
+            Func<TFrom, TTo> convert)
+            where TFrom : class
+            where TTo : class
+        {
+            return new ShortPaginatedResult<TTo>(
+                shortPaginatedResult.Page,
+                shortPaginatedResult.PerPage,
+                shortPaginatedResult.HasNextPage,
+                shortPaginatedResult.Data.Select(convert).ToList());
         }
     }
 }
