dd835b35db231643b456910b4e75ce0197f497da
DESKTOP-KT6GSMQ\Ilya <win7user10@yandex.ru>
2024-03-20 21:57:31 +0300

    add pagination extensions for collections

---
+++ src/Laraue.Core.DataAccess/Extensions/PaginationExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Laraue.Core.DataAccess.Contracts;
+using Laraue.Core.DataAccess.Utils;
+
+namespace Laraue.Core.DataAccess.Extensions;
+
+/// <summary>
+/// Pagination extensions for the sequences.
+/// </summary>
+public static class PaginationExtensions
+{
+    /// <summary>
+    /// Create full pagination for the collection.
+    /// </summary>
+    /// <param name="query"></param>
+    /// <param name="request"></param>
+    /// <typeparam name="TEntity"></typeparam>
+    /// <returns></returns>
+    public static IFullPaginatedResult<TEntity> FullPaginate<TEntity>(
+        this ICollection<TEntity> query,
+        IPaginatedRequest request)
+        where TEntity : class
+    {
+        var total = query.Count;
+        var skip = request.Page * request.PerPage;
+
+        var data = query.Skip(skip)
+            .Take(request.PerPage)
+            .ToList();
+
+        return new FullPaginatedResult<TEntity>(request.Page, request.PerPage, total, data);
+    }
+        
+    /// <summary>
+    /// Create short pagination for the sequence.
+    /// </summary>
+    public static IShortPaginatedResult<TEntity> ShortPaginate<TEntity>(
+        this IEnumerable<TEntity> query,
+        IPaginatedRequest request)
+        where TEntity : class
+    {
+        var skip = request.Page * request.PerPage;
+
+        var data = query.Skip(skip)
+            .Take(request.PerPage + 1)
+            .ToList();
+
+        return ShortPaginatedResultUtil.BuildResult(request, data);
+    }
+}
