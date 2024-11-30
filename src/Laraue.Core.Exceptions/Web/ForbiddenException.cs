11f669260fdd7f006fee8999b28af5d96e0ecc48
DESKTOP-KT6GSMQ\Ilya <win7user10@yandex.ru>
2024-11-30 12:49:26 +0300

    add ForbiddenException

---
+++ src/Laraue.Core.Exceptions/Web/ForbiddenException.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+
+namespace Laraue.Core.Exceptions.Web
+{
+    /// <summary>
+    /// Exception with 406 code
+    /// </summary>
+    public class ForbiddenException : HttpException
+    {
+        /// <summary>
+        /// Initializes a new instance of <see cref="ForbiddenException"/>.
+        /// </summary>
+        /// <param name="message"></param>
+        public ForbiddenException(string message)
+            : base(HttpStatusCode.Forbidden, message)
+        {
+        }
+    }
+}
